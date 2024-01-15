1cea01571e49cd85c815b8e9102db6f693aa2b29
Amilcar-afk <fernandesamilcar28@gmail.com>
2024-01-15 13:50:46 +0100

    cas_pratique chapitre 2 ok

--- client/src/app/@shared/components/menu/menu.component.ts
+++ client/src/app/@shared/components/menu/menu.component.ts
@@ -4,6 +4,7 @@ import { FolderService } from '../../../@core/services/folder.service';
 import { Folder } from '../../../@core/models/Folder';
 import { DocumentService } from '../../../@core/services/document.service';
 import { Document } from '../../../@core/models/Document';
+import { of, catchError, tap} from 'rxjs';
 
 @Component({
   selector: 'app-menu',
@@ -13,11 +14,17 @@ import { Document } from '../../../@core/models/Document';
 export class MenuComponent {
   visible: boolean = false;
   folderForm: FormGroup = new FormGroup({});
+  modalVisible: boolean = false;
+  modalTitle: string = 'Info';
+  modalMessage: string = '';
+  modalVisibleError: boolean = false;
+  modalTitleError: string = 'Info'
+  modalMessageError: string = '';
   
   constructor(
     private fb: FormBuilder,
     private folderService: FolderService,
-    private documentService: DocumentService
+    private documentService: DocumentService,
   ) { }
 
   ngOnInit(): void {
@@ -42,15 +49,66 @@ export class MenuComponent {
   }
 
   onUpload(event: any) {
-    console.log(event.files);
-    this.documentService.createDocument(event.files[0]).subscribe({
-      next: (document: Document) => {
-        console.log(document);
+    const file = event.files[1];
+    this.documentService.createDocument(file).pipe(
+      tap(
+        () => this.openModal('Document creation in progress...'),
+      ),
+      catchError((error) => {
+        this.openModalError('Document creation failed.');
+        console.error('Error during document creation: ', error);
+        return of(null);
+      })
+    ).subscribe({
+      next: (document: Document | null) => {
+        if (document) {
+          console.log('Document created successfully: ', document);
+        } else {
+          console.log('Document creation failed.');
+        }
       },
       error: (err) => {
         console.error(err);
       }
-    })
+    });
+  }
+
+  barProgress() {
+    console.log("progress");
+    const progress = document.getElementById('progress') as HTMLDivElement;
+    console.log(progress);
+    const h2 = document.getElementById('h2_progress') as HTMLHeadingElement;
+    console.log(h2);
+  
+    let i = 0;
+  
+    function animateProgress() {
+      if (i < 100) {
+        i++;
+        h2.innerHTML = i + '%';
+        progress.style.width = i + '%';
+        requestAnimationFrame(animateProgress);
+      }
+    }
+  
+    animateProgress();
+  }
+
+  openModal(message: string) {
+    this.modalMessage = message;
+    this.modalVisible = true;
+    setTimeout(() => this.barProgress(), 1000);
+    setTimeout(() => location.reload(), 3000)
+  }
+
+  openModalError(message: string) {
+    this.modalMessageError = message;
+    this.modalVisibleError = true;
+  }
+
+  closeModal() {
+    this.modalVisible = false;
+    this.modalVisibleError = false;
   }
 
   createFolder(folder: Folder): void {
