5c71504a58bbeccd52bab80d685295dae8f878ec
Pietrucci-Blacher <maxime.pietrucci@gmail.com>
2024-01-20 11:29:46 +0100

    Feat: Add refactored companents with folders

--- client/src/app/@shared/components/menu/menu.component.ts
+++ client/src/app/@shared/components/menu/menu.component.ts
@@ -1,25 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FolderService } from '../../../@core/services/folder.service';
 import { Folder } from '../../../@core/models/Folder';
 import { DocumentService } from '../../../@core/services/document.service';
 import { Document } from '../../../@core/models/Document';
-import { of, catchError, tap} from 'rxjs';
+import { of, catchError, tap, BehaviorSubject, Subject} from 'rxjs';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit{
   visible: boolean = false;
   folderForm: FormGroup = new FormGroup({});
-  modalVisible: boolean = false;
   modalTitle: string = 'Info';
   modalMessage: string = '';
-  modalVisibleError: boolean = false;
   modalTitleError: string = 'Info'
   modalMessageError: string = '';
+  uploadProgress$: BehaviorSubject<number> = new BehaviorSubject(0);
+  modalVisibility$: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  modalErrorVisibility$: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  folderCreated$: Subject<Folder> = new Subject();
+  errorOccurred$: Subject<string> = new Subject();
 
   constructor(
     private fb: FormBuilder,
@@ -51,9 +54,7 @@ export class MenuComponent {
   onUpload(event: any) {
     const file = event.files[0];
     this.documentService.createDocument(file).pipe(
-      tap(
-        () => this.openModal('Document creation in progress...'),
-      ),
+      tap(() => this.openModal('Document creation in progress...')),
       catchError((error) => {
         this.openModalError('Document creation failed.');
         console.error('Error during document creation: ', error);
@@ -71,6 +72,7 @@ export class MenuComponent {
         console.error(err);
       }
     });
+    this.updateUploadProgress();
   }
 
   barProgress() {
@@ -94,31 +96,44 @@ export class MenuComponent {
     animateProgress();
   }
 
+  updateUploadProgress() {
+    let progress = 0;
+    const interval = setInterval(() => {
+      if (progress < 100) {
+        progress += 5;
+        this.uploadProgress$.next(progress);
+      } else {
+        clearInterval(interval);
+      }
+    }, 100);
+  }
+
   openModal(message: string) {
     this.modalMessage = message;
-    this.modalVisible = true;
+    this.modalVisibility$.next(true);
     setTimeout(() => this.barProgress(), 1000);
-    setTimeout(() => location.reload(), 3000)
+    setTimeout(() => location.reload(), 3000);
   }
 
   openModalError(message: string) {
     this.modalMessageError = message;
-    this.modalVisibleError = true;
+    this.modalErrorVisibility$.next(true);
   }
-
   closeModal() {
-    this.modalVisible = false;
-    this.modalVisibleError = false;
+    this.modalVisibility$.next(false);
+    this.modalErrorVisibility$.next(false);
   }
 
   createFolder(folder: Folder): void {
     this.folderService.createFolder(folder).subscribe({
       next: (folder: Folder) => {
+        this.folderCreated$.next(folder);
         console.log(folder);
       },
       error: (err) => {
+        this.errorOccurred$.next('Error creating folder');
         console.error(err);
       }
-    })
+    });
   }
 }
